b465ceb1929e0ee904f5be4f105cefc1a1ae88ab
Luis Daniel Acosta <luisdanielacostacamrgo@gmail.com>
2022-07-22 14:37:29 +0200

    Eclipse-Proyecto.Final

---
+++ APIRest.MVC-ProyectoFinal/src/main/java/com/Proyectofinalwed/service/UsuarioServiceImpl.java
@@ -0,0 +1,54 @@
+package com.Proyectofinalwed.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.Proyectofinalwed.entity.Usuario;
+import com.Proyectofinalwed.repository.UsuarioDao;
+
+
+
+	
+	@Service
+	public class UsuarioServiceImpl implements UsuarioService{ 
+		
+		  @Autowired
+		    private UsuarioDao usuarioDao;
+
+		    @Override
+		    @Transactional(readOnly = true)
+		    public List<Usuario> mostrarTodos() {
+
+		        return (List<Usuario>) usuarioDao.findAll();
+		    }
+
+			@Override
+			@Transactional(readOnly = true)
+			public Usuario mostrarporId(long id) {
+				// TODO Auto-generated method stub
+				return usuarioDao.findById(id).orElse(null);
+			}
+
+			@Override
+			@Transactional
+			public Usuario guardar(Usuario cliente) {
+				// TODO Auto-generated method stub
+				return usuarioDao.save(cliente);
+			}
+
+			@Override
+			@Transactional
+			public void borrar(long id) {
+				// TODO Auto-generated method stub
+				usuarioDao.deleteById(id);
+			}
+
+			
+
+			
+
+
+}
---
+++ APIRest.MVC-ProyectoFinal/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into usuario(nombre,apellido,email,telefono) values("grecia","morillo","kjifi@rgfnw",1245);
+insert into usuario(nombre,apellido,email,telefono) values("angella","gonzalez","ange@rgfnw",4500);
+insert into usuario(nombre,apellido,email,telefono) values("luis","acosta","ldc@rgfnw",1201);
+insert into usuario(nombre,apellido,email,telefono) values("juan","perez","jperez@rgfnw",1202);
+insert into usuario(nombre,apellido,email,telefono) values("camilo","ortiz","camilo@rgfnw",12
